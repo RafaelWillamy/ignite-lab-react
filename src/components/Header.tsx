40a380ef8d4e4ffd6f99ffa2923cb4c6b9bda0eb
rafaelwillamy <rafaelwillamy@gmail.com>
2022-06-25 13:36:14 -0300

    ignite-lab

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo";
+
+function Header(){
+  return(
+  <header className="items-center w-full justify-center py-5  bg-gray-700 flex border-b border-gray-600">
+    <Logo/>
+  </header>
+ 
+  )
+}
+export default Header;
